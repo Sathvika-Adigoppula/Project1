ec1e1064210da7172895e41c89d0ec90119745ed
Sathvika-Adigoppula <adigoppulasathvika@gmail.com>
2024-03-21 10:09:59 +0530

    hi this the frist file committed

---
+++ Power.java
@@ -0,0 +1,24 @@
+class Power
+{
+public static void main( String[] args)
+{
+	
+	int num=5;
+	int po=0;
+	int temp;
+	int sum=0;
+	for(int i=1;i<=num;i++)
+		{
+			po=5^i;
+			
+			sum=sum+po;
+
+			
+
+		}
+	System.out.println(sum);
+	
+
+}
+
+}
